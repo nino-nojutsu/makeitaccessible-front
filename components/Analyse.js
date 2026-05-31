92ac69d5f801d9a2fca39978c8387e955f402820
Charles Fontaine <fontaine.charles64@gmail.com>
2026-05-31 15:22:54 +0200

    feat(Analyse): delete the call of Issue component + Add useRouter hook from next + redirect to the audit page if results are ok

--- components/Analyse.js
+++ components/Analyse.js
@@ -1,44 +1,67 @@
-// Composant Analyse => affiche URL et envoi la demande d'audit au serveur
+/**
+ ** Composant Analyse : le champ url est passé dans le body de la requête /audit en POST
+ ** Récupère les résultats de l'audit si le backend a bien répondu (data.result à true)
+ ** Enregistre les résultats dans la key/propriété 'audit' dans le localStorage (via reduxjs tooltkit et redux-persist)
+ ** Redirige vers la page audit
+ **/
 
-import { useState } from 'react';
-import styles from '../styles/Home.module.css';
-import Issue from "./Issue.js";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
 
 function Analyse() {
+  /** state **/
+  // stocke l'URL saisie par l'utilisateur
+  const [url, setUrl] = useState("");
 
-// stocke l'URL saisie par l'utilisateur
-  const [url, setUrl] = useState('');
-  const [issues, setIssues] = useState([]);
-
+  /** comportements **/
   // Met à jour l'état à chaque lancement d'analyse
   const handleInputChange = (targetUrl) => {
-      setUrl(targetUrl);
+    setUrl(targetUrl);
   };
 
-  // Envoi de l'URL au backend pour lancer l'analyse
-  const handleSubmit = () => {
+  // Envoi de l'URL au backend en webservice pour lancer l'analyse
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // @nina todo: Open Modale + Loader
+
     fetch(`${process.env.NEXT_PUBLIC_URL}/audit`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ url })
+      body: JSON.stringify({ url }),
     })
       .then((response) => response.json())
       .then((data) => {
         if (data.result) {
-          const issuesList = data.issues.map((issue) => {
-            return <Issue />;
-          });
-          setIssues([...issues, issuesList]);
+          // @nina todo: Close Modale
+          router.push("/audit");
         }
       })
       .catch((error) => console.error(error));
   };
-  
+
+  /** affichage **/
   return (
     <div>
-      <form className={styles.mainAnalyse}  role="search" aria-label="Tester d'accessibilité">
-        <input className={styles.search} id="url-input" name="url" type="url" aria-required="true" placeholder="Url de votre site internet..." value={url} onChange={(e) => handleInputChange(e.target.value)} />
-        <button className={styles.ctaSearch} onClick={handleSubmit} type="submit"> Analyser mon site → </button>
+      <form
+        className={styles.mainAnalyse}
+        onSubmit={handleSubmit}
+        role="search"
+        aria-label="Tester d'accessibilité"
+      >
+        <input
+          className={styles.search}
+          id="url-input"
+          name="url"
+          type="url"
+          aria-required="true"
+          placeholder="Url de votre site internet..."
+          value={url}
+          onChange={(e) => handleInputChange(e.target.value)}
+        />
+        <button className={styles.ctaSearch} type="submit">
+          Analyser mon site →
+        </button>
       </form>
     </div>
   );
