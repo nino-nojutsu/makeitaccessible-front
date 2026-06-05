493cb082e9b8bab82977caab760c1664a44e03d7
Charles Fontaine <fontaine.charles64@gmail.com>
2026-06-05 16:19:31 +0200

    styles: add styles for header et footer, call only one logo and Link next tag to redirect to the homepage

--- components/nav/Header.js
+++ components/nav/Header.js
@@ -1,75 +1,73 @@
-import styles from '../../styles/Header.module.css';
-import Image from 'next/image';
+import styles from "../../styles/Header.module.css";
+import Image from "next/image";
+import Link from "next/link";
 import "antd/dist/antd.css";
-import { Modal } from 'antd';
-import { useState } from 'react';
-import{useSelector} from 'react-redux';
-import SignIn from '../modals/SignIn';
-import SignUp from '../modals/SignUp';
+import { Modal } from "antd";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import SignIn from "../modals/SignIn";
+import SignUp from "../modals/SignUp";
 
 function Header() {
+  const user = useSelector((state) => state.user.value);
 
-        const user = useSelector((state) => state.user.value);
+  const [signIn, setSignIn] = useState(false);
+  const [signUp, setSignUp] = useState(false);
 
-         const [signIn, setSignIn] = useState(false);
-         const [signUp, setSignUp] = useState(false);
+  return (
+    <header className={styles.header} role="banner">
+      <Link href="/">
+        <Image
+          src="/images/logo-MakeItAccessible.svg"
+          alt="logo MakeItAccessible"
+          width={230}
+          height={30}
+          className={styles.logoImage}
+        />
+      </Link>
+      <nav
+        aria-label="S'inscire ou se connecter"
+        className={styles.modalesContainer}
+      >
+        {/* integrate connexion modals */}
+        <button
+          className={styles.btnConnected}
+          onClick={() => {
+            setSignIn(true);
+          }}
+        >
+          Se connecter
+        </button>
 
- /* const handleInputChange = (targetUrl) => {
-    setUrl(targetUrl);
-  };*/
+        <Modal
+          title="Se connecter à votre compte"
+          open={signIn}
+          onCancel={() => setSignIn(false)}
+          footer={null}
+          className={styles.modalesSignIn}
+        >
+          <SignIn />
+        </Modal>
 
-	return (
-		<header className={styles.header} role="banner">
-            <div className={styles.logoWrapper}>
-                <div className={styles.iconWrapper}>
-                <Image
-                    src='/favicon-makeitaccessible.svg'
-                    alt=''
-                    aria-hidden="true"
-                    width={32}
-                    height={32}
-                    className={styles.favicon}/>
-                </div>
-                <div className={styles.logoTextWrapper}>
-                <Image 
-                    src='/images/logo-MakeItAccessible.svg'
-                    alt='logo MakeItAccessible'
-                    width={150}
-                    height={150}
-                    className={styles.logo}/>
-                </div>
-            </div>
-                <nav aria-label="S'inscire ou se connecter" className={styles.modalesContainer}>
-                    {/* integrate connexion modals */}
-                    <button className={styles.btnConnected} onClick={() => {setSignIn(true)}}>
-                        Se connecter
-                    </button>
-                    
-                    <Modal
-                            title="Se connecter à votre compte"
-                            open={signIn}
-                            onCancel={() => setSignIn(false)}
-                            footer={null} className={styles.modalesSignIn}
-                        >
-                            <SignIn />
-                        </Modal>
-                        
-                    <button className={styles.btnRegistration} onClick={() => setSignUp(true)}>
-                        S'inscrire
-                    </button>
-                    
-                    <Modal
-                            title="Créer votre compte"
-                            open={signUp}
-                            onCancel={() => setSignUp(false)}
-                            footer={null} className={styles.modalesSignUp}
-                        >
-                            <SignUp />
-                        </Modal>
+        <button
+          className={styles.btnRegistration}
+          onClick={() => setSignUp(true)}
+        >
+          S'inscrire
+        </button>
 
-                </nav>
-		</header>
-	);
+        <Modal
+          title="Créer votre compte"
+          open={signUp}
+          onCancel={() => setSignUp(false)}
+          footer={null}
+          className={styles.modalesSignUp}
+        >
+          <SignUp />
+        </Modal>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
