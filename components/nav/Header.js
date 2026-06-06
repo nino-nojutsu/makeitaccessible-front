65c63ce12efd9cd267bbd0165c16f3c7b5dc8b7b
Charles Fontaine <fontaine.charles64@gmail.com>
2026-06-06 18:44:48 +0200

    fix(antd): remove the call of antd styles to avoid styles overrides by antd

--- components/nav/Header.js
+++ components/nav/Header.js
@@ -1,7 +1,6 @@
 import styles from "../../styles/Header.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import "antd/dist/antd.css";
 import { Modal } from "antd";
 import { useState } from "react";
 import { useSelector } from "react-redux";
