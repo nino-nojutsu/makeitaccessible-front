b4381e28fefe034aaae2c4d05ec57e8ec6356262
Charles Fontaine <fontaine.charles64@gmail.com>
2026-06-09 00:09:45 +0200

    fix(audit): handle if a user exists or not. Display tests results if a token exists or hide all

--- components/audit/Audit.js
+++ components/audit/Audit.js
@@ -9,6 +9,7 @@ import Filters from './Filters.js';
 
 function Audit() {
   // Récupère les infos de l'audit depuis le store redux (key makeitaccessible stocké en localStorage)
+  const user = useSelector((state) => state.user.value);
   const auditData = useSelector((state) => state.audit.value);
   const router = useRouter();
 
@@ -28,20 +29,6 @@ function Audit() {
   const [selectedImpact, setSelectedImpact] = useState('all'); // all | critical | serious | moderate | minor
 
   /** comportements **/
-
-  // 1. Filtrer par catégorie ou pas : soit on récupère les tests d'une catégorie si un catégorie est séléctionnée soit on stock tous les tests (ternaire)
-  const filteredByCat = selectedCat ? audit.tests.filter(test => test.category === selectedCat) : audit.tests;
-  // console.log('filteredByCat', filteredByCat);
-
-  // 2. Pré-filtrage par type à partir des catégories filtrées (par catégorie sélectionnée ou toutes catégories confondues)
-
-  // Depuis un filtrage de cat, récupère les violations (array non vide)
-  const violations = filteredByCat.length > 0 && filteredByCat.filter(test => test.violations.length > 0);
-  // Depuis un filtrage de cat, récupère les incomplete (array non vide)
-  const incomplete = filteredByCat.length > 0 && filteredByCat.filter(test => test.incomplete.length > 0);
-  // Depuis un filtrage de cat, récupère les passes (array non vide)
-  const passes = filteredByCat.length > 0 && filteredByCat.filter(test => test.passes.length > 0);
-
   // Fonction (idf) de filtrage par categorie déclenché un click sur une cat (composant Catégorie + idf)
   const handleFilteredByCat = (category) => {
     category === 'allCats' ? setSelectedCat('') : setSelectedCat(category);
@@ -60,63 +47,81 @@ function Audit() {
     setSelectedImpact(value);
   }
 
-  // On crée un tableau de composants Category + Inverse Data Flow passé en props (handleFilteredByCat) depuis audit.tests
-  const categoriesList = audit.tests.length > 0 && audit.tests.map((data, i) => {
-    // Affiche la class isSelected ou pas
-    const isSelected = data.category === selectedCat;
-    // Compte le nombre total d'anomalie : incomplete + violations
-    const totalIssues = data.incomplete.length + data.violations.length;
-    // Récupère le nombre de tests validés
-    const totalPasses = data.passes.length;
-    // A uniquement des non applicables dans les résultats
-    const hasInapplicable = data.inapplicable.length > 0 && totalIssues + totalPasses === 0;
-    // Composant Catégorie qui filtre par type et par criticité (passage par l'idf handleFilteredByCat)
-    return <Category
-      key={i}
-      category={data.category}
-      handleFilteredByCat={handleFilteredByCat}
-      className={isSelected ? styles.isSelected : null}
-      totalIssues={totalIssues}
-      totalPasses={totalPasses}
-      hasInapplicable={hasInapplicable} />
-  });
+  if (user.token) {
+    // 1. Filtrer par catégorie ou pas : soit on récupère les tests d'une catégorie si un catégorie est séléctionnée soit on stock tous les tests (ternaire)
+    const filteredByCat = selectedCat ? audit.tests.filter(test => test.category === selectedCat) : audit.tests;
+    // console.log('filteredByCat', filteredByCat);
+
+    // 2. Pré-filtrage par type à partir des catégories filtrées (par catégorie sélectionnée ou toutes catégories confondues)
+
+    // Depuis un filtrage de cat, récupère les violations (array non vide)
+    const violations = filteredByCat.length > 0 && filteredByCat.filter(test => test.violations.length > 0);
+    // Depuis un filtrage de cat, récupère les incomplete (array non vide)
+    const incomplete = filteredByCat.length > 0 && filteredByCat.filter(test => test.incomplete.length > 0);
+    // Depuis un filtrage de cat, récupère les passes (array non vide)
+    const passes = filteredByCat.length > 0 && filteredByCat.filter(test => test.passes.length > 0);
+
+    // On crée un tableau de composants Category + Inverse Data Flow passé en props (handleFilteredByCat) depuis audit.tests
+    const categoriesList = audit.tests.length > 0 && audit.tests.map((data, i) => {
+      // Affiche la class isSelected ou pas
+      const isSelected = data.category === selectedCat;
+      // Compte le nombre total d'anomalie : incomplete + violations
+      const totalIssues = data.incomplete.length + data.violations.length;
+      // Récupère le nombre de tests validés
+      const totalPasses = data.passes.length;
+      // A uniquement des non applicables dans les résultats
+      const hasInapplicable = data.inapplicable.length > 0 && totalIssues + totalPasses === 0;
+      // Composant Catégorie qui filtre par type et par criticité (passage par l'idf handleFilteredByCat)
+      return <Category
+        key={i}
+        category={data.category}
+        handleFilteredByCat={handleFilteredByCat}
+        className={isSelected ? styles.isSelected : null}
+        totalIssues={totalIssues}
+        totalPasses={totalPasses}
+        hasInapplicable={hasInapplicable} />
+    });
+  }
 
   /** affichage **/
   return (
-    <div className={styles.auditContainer}>
-      {/* Composant Catégorie qui filtre par thématique (Images, Cadres, Couleurs, etc...) */}
-      { categoriesList.length > 0 && (
-        <aside role="navigation" className={styles.categoriesList}>
-          <ol className={styles.listGroup}>
-            {categoriesList}
-          </ol>
-          {
-            selectedCat &&
-            <span className={styles.showAll} onClick={() => handleFilteredByCat('allCats')}>
-              Afficher tous les résultats
-            </span>
-          }
-        </aside>
-      )}
+    <>
+      {user.token &&
+      <div className={styles.auditContainer}>
+        {/* Composant Catégorie qui filtre par thématique (Images, Cadres, Couleurs, etc...) */}
+        { categoriesList.length > 0 && (
+          <aside role="navigation" className={styles.categoriesList}>
+            <ol className={styles.listGroup}>
+              {categoriesList}
+            </ol>
+            {
+              selectedCat &&
+              <span className={styles.showAll} onClick={() => handleFilteredByCat('allCats')}>
+                Afficher tous les résultats
+              </span>
+            }
+          </aside>
+        )}
       
-      <div className={styles.auditResults}>
-        {/* Composant Filtres qui filtre par type et par criticité (passage par les idf handleFilteredByType et handleFilteredByImpact) */}
-        {
-          (violations.length > 0 || incomplete.length > 0) &&
-          <Filters handleFilteredByType={handleFilteredByType} handleFilteredByImpact={handleFilteredByImpact} selectedCat={selectedCat} />
-        }
-          
-        {/* Composant Results qui gère le switch entre les 3 sections (groupe les tests par violations, incomplete et passes) selon les filtres sélectionnés avec selectedType et selectedImpact */}
-        {
-          violations.length > 0 || incomplete.length > 0 ?
-          <Results violations={violations} incomplete={incomplete} passes={passes} selectedType={selectedType} selectedImpact={selectedImpact} /> :
-          <>
-            <div className={styles.noResults}>Nous n'avons pas trouvé d'anomalies pour cette thématique. <br />Bravo ! 😊</div>
-            <Results passes={passes} selectedType={selectedType} selectedImpact={selectedImpact} />
-          </>
-        }
-      </div>
-    </div>
+        <div className={styles.auditResults}>
+            {/* Composant Filtres qui filtre par type et par criticité (passage par les idf handleFilteredByType et handleFilteredByImpact) */}
+            {
+              (violations.length > 0 || incomplete.length > 0) &&
+              <Filters handleFilteredByType={handleFilteredByType} handleFilteredByImpact={handleFilteredByImpact} selectedCat={selectedCat} />
+            }
+              
+            {/* Composant Results qui gère le switch entre les 3 sections (groupe les tests par violations, incomplete et passes) selon les filtres sélectionnés avec selectedType et selectedImpact */}
+            {
+              violations.length > 0 || incomplete.length > 0 ?
+              <Results violations={violations} incomplete={incomplete} passes={passes} selectedType={selectedType} selectedImpact={selectedImpact} /> :
+              <>
+                <div className={styles.noResults}>Nous n'avons pas trouvé d'anomalies pour cette thématique. <br />Bravo ! 😊</div>
+                <Results passes={passes} selectedType={selectedType} selectedImpact={selectedImpact} />
+              </>
+            }
+        </div>
+      </div>}
+    </>
   )
 }
 
