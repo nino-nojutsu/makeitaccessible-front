c769aa9d1091657c9e0e1638af9567e24f3876af
Annie <anniesyed2005@gmail.com>
2026-06-11 19:36:48 +0200

    mise à jour du fichier components/audit/Test.js

--- components/audit/Test.js
+++ components/audit/Test.js
@@ -1,4 +1,5 @@
 import styles from '../../styles/Tests.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 
 // Impact de criticité (obligé de traduire car axe-core les envoie en anglais :/ alors que la locale est bien fr en back)
@@ -12,6 +13,14 @@ const impactLabel = {
 // Test affiche une seule règle axe-core (description, impact, html, etc....)
 function Test({ status, description, help, impact, html, tags, id }) {
   /** affichage **/
+
+  const [retirerUnCritere, setRetirerUnCritere] = useState('');
+  const [ajouterUnCommentaire, setAjouterUnCommentaire] = useState('');
+
+   const handleSubmit = () => {
+   
+ };
+
   return (
     <div className={`${styles.testTile} ${styles.status} ${styles[`status-${status}`]}`}>
       {status === 'success' &&
@@ -21,6 +30,18 @@ function Test({ status, description, help, impact, html, tags, id }) {
         {impact === null ? 'Impact non communiqué' : impactLabel[impact]}
       </span>
       <p>{description}</p>
+      <div>
+        <input type="checkbox" id={id} name={id} value={id} onChange={(e) => setRetirerUnCritere(e.target.value)} />
+        <button className="btn btn-primary" onClick={() => setRetirerUnCritere(html)}>
+          <FontAwesomeIcon icon="trash-alt" />
+        </button>
+        <div>
+          <input type="text" id={`comment-${id}`} name={`comment-${id}`} value={ajouterUnCommentaire} onChange={(e) => setAjouterUnCommentaire(e.target.value)} />
+          <button className="btn btn-primary" onClick={handleSubmit}>
+            <FontAwesomeIcon icon="comment-dots" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
