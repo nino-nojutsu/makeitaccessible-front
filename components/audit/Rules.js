8dc75cf566b75364787b9c1ad7e5a1266a3f365d
Charles Fontaine <fontaine.charles64@gmail.com>
2026-06-15 08:10:22 +0200

    feat(rules): add comment

--- components/audit/Rules.js
+++ components/audit/Rules.js
@@ -2,13 +2,14 @@ import styles from '../../styles/Rules.module.css';
 import { useState } from "react";
 import Test from './Test.js';
 
-// Tests reçoit une catégorie et ses règles axe-core filtrées par type (rules)
-// Affiche le titre de la catégorie + la liste des règles (liste composants Test)
+// Rules reçoit une catégorie et ses règles axe-core filtrées par type
+// Affiche le titre de la catégorie + la liste des règles (liste de composants Test.js)
 function Rules({ category, rules, selectedImpact, status, nodes }) {
   // console.log('status', status);
   // console.log('rules', rules);
   // console.log('selectedImpact', selectedImpact);
 
+  // Filtre par impact si un impact est sélectionné (critical, major, minor)
   const filteredByImpact = selectedImpact !== 'all' ? rules.filter(rule => rule.impact === selectedImpact) : rules;
   const testsList = filteredByImpact.map((test, i) => {
     return <Test key={i} status={status} {...test} />
