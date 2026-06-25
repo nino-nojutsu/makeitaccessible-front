2e2fc79aa76ef93f72bfb83e004dbf4ad284a536
Charles Fontaine <fontaine.charles64@gmail.com>
2026-06-25 20:33:47 +0200

    fix(results): add optional chaining to prevent errors when there are no violations, incomplete

--- components/audit/Results.js
+++ components/audit/Results.js
@@ -8,25 +8,25 @@ function Results({ violations, incomplete, passes, processed, selectedType, sele
   /** comportements **/
 
   /* Groupe les rules par violations */
-  const violationsRulesList = violations.length > 0 && violations.map((testDoc, i) => {
+  const violationsRulesList = violations?.length > 0 && violations?.map((testDoc, i) => {
     const rules = testDoc.violations.filter(rule => rule.status !== 'validated' && rule.status !== 'ignored' && rule.status !== 'reviewed');
     return <Rules key={i} testId={testDoc._id} type="violations" category={testDoc.category} rules={rules} selectedImpact={selectedImpact} alert={'error'} />;
   });
 
   /* Groupe les rules par incomplete */
-  const incompleteRulesList = incomplete.length > 0 && incomplete.map((testDoc, i) => {
+  const incompleteRulesList = incomplete?.length > 0 && incomplete?.map((testDoc, i) => {
     const rules = testDoc.incomplete.filter(rule => rule.status !== 'validated' && rule.status !== 'ignored' && rule.status !== 'reviewed');
     return <Rules key={i} testId={testDoc._id} type="incomplete" category={testDoc.category} rules={rules} selectedImpact={selectedImpact} alert={'warning'} />;
   });
 
   /* Groupe les rules par passes */
-  const passesRulesList = passes.length > 0 && passes.map((testDoc, i) => {
+  const passesRulesList = passes?.length > 0 && passes?.map((testDoc, i) => {
     const rules = testDoc.passes.filter(rule => rule.status !== 'validated' && rule.status !== 'ignored' && rule.status !== 'reviewed');
     return <Rules key={i} testId={testDoc._id} type="passes" category={testDoc.category} rules={testDoc.passes} selectedImpact={selectedImpact} alert={'success'} />;
   });
 
   /* Groupe les rules traitées (validées + ignored) */
-  const processedRulesList = processed.length > 0 && processed.map((testDoc, i) => {
+  const processedRulesList = processed?.length > 0 && processed?.map((testDoc, i) => {
     const rulesValidated = [
       ...testDoc.violations.filter(rule => rule.status === 'validated'),
       ...testDoc.incomplete.filter(rule => rule.status === 'validated'),
@@ -85,21 +85,21 @@ function Results({ violations, incomplete, passes, processed, selectedType, sele
       return (
         <>
           {
-            processed.length > 0 && 
+            processed?.length > 0 && 
             <><h3>Anomalies traitées</h3> {processedRulesList}</>
           }
           {
-            violations.length > 0 && 
+            violations?.length > 0 && 
             <><h3>Anomalies</h3> {violationsRulesList}</>
           }
 
           {
-            incomplete.length > 0 &&
+            incomplete?.length > 0 &&
             <><h3>Anomalies incomplètes</h3> {incompleteRulesList}</>
           }
 
           {
-            passes.length > 0 &&
+            passes?.length > 0 &&
             <><h3>Validées</h3> {passesRulesList}</>
           }
         </>
